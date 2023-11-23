16a74dc3cb074d3c5e90506756a5a66762dc6a97
cjyyiii <119089204+cjyyiii@users.noreply.github.com>
2023-11-23 17:00:12 +0800

    Update lang.h

--- lang.h
+++ lang.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "lib.h"
 
 enum BinOpType {
   T_PLUS,
@@ -31,9 +30,6 @@ enum ExprType {
   T_VAR,
   T_BINOP,
   T_UNOP,
-  T_DEREF,
-  T_ADDR_OF,
-  T_FUNC
 };
 
 enum CmdType {
@@ -42,25 +38,6 @@ enum CmdType {
   T_SEQ,
   T_IF,
   T_WHILE,
-  T_FOR,
-  T_DO_WHILE,
-  T_PROC,
-  T_BREAK,
-  T_CONTINUE,
-  T_RETURN
-};
-
-enum GlobItemType {
-  T_FUNC_DEF,
-  T_PROC_DEF,
-  T_GLOB_VAR
-};
-
-struct expr;
-
-struct expr_list {
-  struct expr * data;
-  struct expr_list * next;
 };
 
 struct expr {
@@ -70,89 +47,36 @@ struct expr {
     struct {char * name; } VAR;
     struct {enum BinOpType op; struct expr * left; struct expr * right; } BINOP;
     struct {enum UnOpType op; struct expr * arg; } UNOP;
-    struct {struct expr * arg; } DEREF;
-    struct {struct expr * arg; } ADDR_OF;
-    struct {char * name; struct expr_list * args; } FUNC;
   } d;
 };
 
 struct cmd {
   enum CmdType t;
   union {
-    struct {char * name; struct cmd * body; } DECL;
+    struct {char * name; } DECL;
     struct {struct expr * left; struct expr * right; } ASGN;
     struct {struct cmd * left; struct cmd * right; } SEQ;
     struct {struct expr * cond; struct cmd * left; struct cmd * right; } IF;
     struct {struct expr * cond; struct cmd * body; } WHILE;
-    struct {struct cmd * init; struct expr * cond;
-            struct cmd * incr; struct cmd * body; } FOR;
-    struct {struct cmd * body; struct expr * cond; } DO_WHILE;
-    struct {char * name; struct expr_list * args; } PROC;
-    struct {void * none; } BREAK;
-    struct {void * none; } CONTINUE;
-    struct {void * none; } RETURN;
   } d;
 };
 
-struct var_list {
-  char * name;
-  struct var_list * next;
-};
-
-struct glob_item {
-  enum GlobItemType t;
-  union {
-    struct {char * name; struct var_list * args; struct cmd * body; } FUNC_DEF;
-    struct {char * name; struct var_list * args; struct cmd * body; } PROC_DEF;
-    struct {char * name;} GLOB_VAR;
-  } d;
-};
-
-struct glob_item_list {
-  struct glob_item * data;
-  struct glob_item_list * next;
-};
-
-struct expr_list * TENil();
-struct expr_list * TECons(struct expr * data, struct expr_list * next);
 struct expr * TConst(unsigned int value);
 struct expr * TVar(char * name);
-struct expr * TBinOp(enum BinOpType op, struct expr * left,
-                     struct expr * right);
+struct expr * TBinOp(enum BinOpType op, struct expr * left, struct expr * right);
 struct expr * TUnOp(enum UnOpType op, struct expr * arg);
-struct expr * TDeref(struct expr * arg);
-struct expr * TAddrOf(struct expr * arg);
-struct expr * TFunc(char * name, struct expr_list * args);
-struct cmd * TDecl(char * name, struct cmd * body);
+struct cmd * TDecl(char * name);
 struct cmd * TAsgn(struct expr * left, struct expr * right);
 struct cmd * TSeq(struct cmd * left, struct cmd * right);
 struct cmd * TIf(struct expr * cond, struct cmd * left, struct cmd * right);
 struct cmd * TWhile(struct expr * cond, struct cmd * body);
-struct cmd * TFor(struct cmd * init, struct expr * cond,
-                  struct cmd * incr, struct cmd * body);
-struct cmd * TDoWhile(struct cmd * body, struct expr * cond);
-struct cmd * TProc(char * name, struct expr_list * args);
-struct cmd * TBreak();
-struct cmd * TContinue();
-struct cmd * TReturn();
-struct var_list * TVNil();
-struct var_list * TVCons(char * name, struct var_list * next);
-struct glob_item * TGlobVar(char * name);
-struct glob_item * TFuncDef(char * name, struct var_list * args,
-                            struct cmd * body);
-struct glob_item * TProcDef(char * name, struct var_list * args,
-                            struct cmd * body);
-struct glob_item_list * TGNil();
-struct glob_item_list * TGCons(struct glob_item * data,
-                               struct glob_item_list * next);
 
 void print_binop(enum BinOpType op);
 void print_unop(enum UnOpType op);
 void print_expr(struct expr * e);
-void print_expr_list(struct expr_list * es);
 void print_cmd(struct cmd * c);
-void print_var_list(struct var_list * vs);
-void print_glob_item(struct glob_item * g);
-void print_glob_item_list(struct glob_item_list * gs);
+
+unsigned int build_nat(char * c, int len);
+char * new_str(char * str, int len);
 
 #endif // LANG_H_INCLUDED
